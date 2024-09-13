f4f0d0d3ea32d335a3da21c21397f4e2a7e837c0
Walter Guillioli <wguillioli@gmail.com>
2024-09-12 17:14:56 -0700

    up to rpart eda

--- scripts/earthquake_damage.R
+++ scripts/earthquake_damage.R
@@ -11,6 +11,13 @@ rm(list = ls())
 setwd("C:/GitHub/earthquake_damage/")
 
 require(tidyverse)
+require(corrplot)
+
+get_percentiles <- function(column){
+  print(quantile(column, 
+                 prob = c(0, 0.01, 0.05, 0.10, 0.25, 0.50, 
+                          0.75, 0.90, 0.95, 0.99, 1)))
+}
 
 
 # --------------------------------------------------------------------------
@@ -36,6 +43,7 @@ train$dataset <- "train"
 
 test <- test_values
 test$dataset <- "test"
+test$damage_grade <- 999 # to avoid NAs and will ignore later
                   
 d <- bind_rows(train, test)
 
@@ -48,7 +56,14 @@ sum(is.na(d))
 
 mv <- sapply(d, function(df){sum(is.na(df)==TRUE);})
 mv <- data.frame(mv, variable = names(mv))
-mv %>% arrange(desc(mv)) #good, only damage_grade for test group has                           
+mv %>% arrange(desc(mv)) #no missing values    
+
+# clean a bit
+rm("train", 
+   "test",
+   "train_labels",
+   "train_values",
+   "test_values")
 
 
 # --------------------------------------------------------------------------
@@ -85,7 +100,10 @@ vars_numeric <- c("geo_level_1_id",
                   "geo_level_3_id",
                   "age",
                   "area_percentage",                        
-                  "height_percentage")
+                  "height_percentage"
+                  )
+                  # "count_floors_pre_eq", #discrete?
+                  # "count_families") #discrete?
 
 vars_discrete <- c("land_surface_condition",                 
                    "foundation_type",
@@ -94,7 +112,24 @@ vars_discrete <- c("land_surface_condition",
                    "other_floor_type",
                    "position", 
                    "plan_configuration",
-                   "legal_ownership_status")
+                   "legal_ownership_status",
+                   "count_floors_pre_eq", #num?
+                   "count_families") #num?
+
+# correlation among numeric vars?
+r <- d %>%
+  select(-all_of(vars_discrete),-building_id, -dataset, count_floors_pre_eq,
+         count_families) %>%
+  cor()
+
+round(r,2)
+
+r_df <- data.frame(r)
+r_df$x <- row.names(r_df)
+
+write_csv(r_df, 
+          file = "./temp/r_df.csv") #df to see in xls
+
 
 # iterate over all binary variables and identify which have power of prediction
 # and perform data prep as needed(binning)
@@ -110,38 +145,40 @@ for (i in 2:ncol(df_binary)){
   print(round(prop.table(t, 2),2))
 }
 
-# 2: has_superstructure_adobe_mud - good power
-# 3: has_superstructure_mud_mortar_stone - great power   
-# 4: has_superstructure_stone_flag - small power    
-# 5: has_superstructure_cement_mortar_stone - tiny power  
-# 6: has_superstructure_mud_mortar_brick - small power
-# 7: has_superstructure_cement_mortar_brick - big power 
-# 8: has_superstructure_timber - some power   
-# 9: has_superstructure_bamboo - small power   
-# 10: has_superstructure_rc_non_engineered - some power   
-# 11: has_superstructure_rc_engineered good power  
-# 12: has_superstructure_other - tiny power   
-# 13: has_secondary_use - some power  
-# 14: has_secondary_use_agriculture - tiny power   
-# 15: has_secondary_use_hotel - some power  
-# 16: has_secondary_use_rental - tiny power   
-# 17: has_secondary_use_institution - no power  
-# 18: has_secondary_use_school - no power   
-# 19: has_secondary_use_industry - no power   
-# 20: has_secondary_use_health_post - no power  
-# 21: has_secondary_use_gov_office - no power   
-# 22: has_secondary_use_use_police - no power  
-# 23: has_secondary_use_other - no power
+rm("df_binary")
+
+# summary of potential for prediction of binary variables with power S/M/L?
+# 2: has_superstructure_adobe_mud - S
+# 3: has_superstructure_mud_mortar_stone - L
+# 4: has_superstructure_stone_flag - S
+# 5: has_superstructure_cement_mortar_stone - XS
+# 6: has_superstructure_mud_mortar_brick - S
+# 7: has_superstructure_cement_mortar_brick - L
+# 8: has_superstructure_timber - M
+# 9: has_superstructure_bamboo - S
+# 10: has_superstructure_rc_non_engineered - M
+# 11: has_superstructure_rc_engineered - M
+# 12: has_superstructure_other - XS
+# 13: has_secondary_use - S
+# 14: has_secondary_use_agriculture - S
+# 15: has_secondary_use_hotel - S
+# 16: has_secondary_use_rental - XS
+# 17: has_secondary_use_institution - NO 
+# 18: has_secondary_use_school - NO
+# 19: has_secondary_use_industry - NO
+# 20: has_secondary_use_health_post - NO
+# 21: has_secondary_use_gov_office - NO
+# 22: has_secondary_use_use_police - NO
+# 23: has_secondary_use_other - NO
 
 # remove from df the noise variables with no power
-binary_vars_to_remove <- c("has_secondary_use_institution",
+vars_to_remove_binary <- c("has_secondary_use_institution",
                            "has_secondary_use_school",
                            "has_secondary_use_industry",
                            "has_secondary_use_health_post",
                            "has_secondary_use_gov_office",
                            "has_secondary_use_use_police",
                            "has_secondary_use_other")
-
 
 
 # iterate over all discrete variables and identify which have power of prediction
@@ -158,30 +195,140 @@ for (i in 2:ncol(df_discrete)){
   print(round(prop.table(t, 2),2))
 }
 
-#voy
+rm("df_discrete")
+
+# summary of potential for prediction of discrete variables 
+# tag S/M/H power of prediction
+# 2: land_surface_condition - XS
+# 3: foundation_type - M, bin u_w
+# 4: roof_type - M
+# 5: ground_floor_type - H, bin m_z
+# 6: other_floor_type - H 
+# 7: position - XS, remove?
+# 8: plan_configuration - XS, bin: acfmnos, d, qu?
+# 9: legal_ownership_status - S, bin: rw
+# 10: 10: count_floors_pre_eq - L, bin 4 o 5+  
+# 11: count_families - M, bin 2 o 3+
+
+# no discrete variables to remove for now
+
+# bin variables per EDA above
+# 3: foundation_type - M, bin u_w
+# 5: ground_floor_type - H, bin m_z
+# 8: plan_configuration - XS, bin: acfmnos, d, qu?
+# 9: legal_ownership_status - S, bin: rw
+# 10: 10: count_floors_pre_eq - L, bin 4 o 5+  
+# 11: count_families - M, bin 2 o 3+
+d <- d %>%
+  mutate(foundation_type_imp = case_match(foundation_type,
+                                      c("u", "w") ~ "u_w",
+                                      .default = foundation_type
+                                      ),
+         ground_floor_type_imp = case_match(ground_floor_type,
+                                       c("m", "z") ~ "m_z",
+                                       .default = ground_floor_type
+                                       ),
+         plan_configuration_imp = case_match(plan_configuration,
+                                             c("a", "c", "f", "m", "n", "o", "s") ~ "a_c_f_m_n_o_s",
+                                             .default = plan_configuration
+                                             ),
+         legal_ownership_status_imp = case_match(legal_ownership_status,
+                                                 c("r", "w") ~ "r_w",
+                                                 .default = legal_ownership_status),
+         count_floors_pre_eq_imp = case_when(count_floors_pre_eq >= 4 ~ 4,
+                                             .default = count_floors_pre_eq), #convert to factor later
+         count_families_imp = case_when(count_families >= 3 ~ 3,
+                                    .default = count_families) #convert to factor later
+         )
+
+# discrete vars to remove because they were imputed
+vars_to_remove_discrete <- c("foundation_type",
+                             "ground_floor_type",
+                             "plan_configuration",
+                             "legal_ownership_status",
+                             "count_floors_pre_eq",
+                             "count_families")
+
+# iterate over all numeric variables and identify which have power of prediction
+# and perform data prep as needed (trim, scale change)
+
+df_numeric <- d %>% 
+  select(damage_grade, all_of(vars_numeric))
+
+for (i in 2:ncol(df_numeric)){
+  print("--------------------------------------------------")
+  cat(paste0(i, ": ",colnames(df_numeric[i])))
+
+  print(summary(df_numeric[, i]))
+  
+  print(
+    df_numeric %>%
+    group_by(damage_grade) %>%
+    summarise(median = median(df_numeric[, i]),
+#              quantiles = quantile(df_numeric[, i])
+              )
+  )
+  
+  hist(df_numeric[, i], xlab=names(df_numeric[i]))
+  
+  boxplot(df_numeric[, i] ~ df_numeric$damage_grade,
+                xlab=names(df_numeric[i]), horizontal = TRUE)
+  
+}
+
+rm("df_numeric")
+
+# summary of what was observed in loop and modify var as needed
+
+# "geo_level_1_id",                        
+# 0-30, median 12, not normal but can't transform - bin?, L power
+
+# "geo_level_2_id",
+# 0-1427, median 701, can't transform - remove no power.
+
+# "geo_level_3_id",
+# 0-12567, median 6271 - can't transform - remove no power
+
+# "age",
+# 0-995, median 15, need to trim due to outliers
+get_percentiles(d$age) #cap at 100 (p99)
+d$age_imp <- ifelse(d$age > 100, 100, d$age)
+
+boxplot(d$age_imp ~ d$damage_grade,horizontal = TRUE)
+# not normal but older buildings tend to get more damaged - some power
+
+# "area_percentage",              
+get_percentiles(d$area_percentage) # 1-100, median 7, cap p95 = 16
+d$area_percentage_imp <- ifelse(d$area_percentage > 16, 16, d$area_percentage)
+boxplot(d$area_percentage_imp ~ d$damage_grade,horizontal = TRUE)
+# some power, damage=1 has higher median, # normal-ish
+
+# "height_percentage",
+get_percentiles(d$height_percentage) # 2-32, median 5, cap p99 = 11
+d$height_percentage_imp <- ifelse(d$height_percentage > 11, 11, d$height_percentage)
+boxplot(d$height_percentage_imp ~ d$damage_grade,horizontal = TRUE)
+# tiny power but medians are similar. Will keep but unlikely a good pred
+
+vars_to_remove_numeric <- c("geo_level_2_id", 
+                            "geo_level_3_id", 
+                            "age", "area_percentage", 
+                            "height_percentage")
+
+d <- d %>%
+  select(-all_of(c(vars_to_remove_numeric, 
+                   vars_to_remove_discrete,
+                   vars_to_remove_binary)))
 
 
-# no change
-"building_id"
-
-# y
-"damage_grade"
 
 
-# tbd
-"count_floors_pre_eq"
-"count_families"  
-"dataset" 
+# remove corr predictors using caret, y ver primero
 
 # coece variables to right type                 
-
 
 # remove all vars from above not useful
 #d <- d %>% 
 #select(-any_of(binary_vars_to_remove))
-
-
-
 
 # caret and tidymodels 
 # tree
@@ -189,4 +336,8 @@ for (i in 2:ncol(df_discrete)){
 # rf/gbm
 # nn
 # kn
+
+#to dos
+# arriba quitar los df_binary y eso conforme no necesito. same with read / loaded data
+
 
